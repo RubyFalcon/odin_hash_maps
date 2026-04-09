db95927f5905c31ce29a5c6a6f75de1f1069ee7f
Tupay <61619620+RubyFalcon@users.noreply.github.com>
2026-04-09 11:46:10 +0300

    feat(HashMap): _hash private method created

---
+++ hash_map.js
@@ -0,0 +1,16 @@
+class HashMap {
+  constructor() {
+    this.capacity = 16;
+    this.loadFactor = 0.75;
+    this.buckets = Array.from({ length: this.capacity }, () => []);
+  }
+
+  _hash(key) {
+    let hashCode = 0 
+    const primeNumber = 31
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i) )% this.capacity;
+    }
+    return hashCode
+  }
+}
