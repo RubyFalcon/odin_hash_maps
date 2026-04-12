b26b445fdea590e47db8f84499293a0bfee05b1a
Tupay <61619620+RubyFalcon@users.noreply.github.com>
2026-04-12 10:48:26 +0300

    feat: add keys, values, entries, and length methods to HashMap

--- hash_map.js
+++ hash_map.js
@@ -26,51 +26,61 @@ class HashMap {
 
   get(key) {
     const index = this._hash(key);
-     if (index < 0 || index >= this.buckets.length) {
+    if (index < 0 || index >= this.buckets.length) {
       throw new Error("Trying to access index out of bounds");
     }
 
     const bucket = this.buckets[index];
-    const existingEntry = bucket.find(entry => entry[0] === key);
+    const existingEntry = bucket.find((entry) => entry[0] === key);
     if (existingEntry) {
-        return existingEntry[1]
+      return existingEntry[1];
     }
-    return null
-    
+    return null;
   }
 
   has(key) {
-    const index = this._hash(key)
-      if (index < 0 || index >= this.buckets.length) {
+    const index = this._hash(key);
+    if (index < 0 || index >= this.buckets.length) {
       throw new Error("Trying to access index out of bounds");
     }
 
-    const bucket =this.buckets[index]
-    const existingEntry = bucket.find(entry => entry[0] === key);
-    
-    return !!existingEntry //returns true/false depending if existing entry is truthy
+    const bucket = this.buckets[index];
+    const existingEntry = bucket.find((entry) => entry[0] === key);
+
+    return !!existingEntry; //returns true/false depending if existing entry is truthy
   }
 
   remove(key) {
-    const index = this._hash(key)
-          if (index < 0 || index >= this.buckets.length) {
+    const index = this._hash(key);
+    if (index < 0 || index >= this.buckets.length) {
       throw new Error("Trying to access index out of bounds");
     }
 
-    const bucket = this.buckets[index]
-    const foundIndex = bucket.findIndex(entry=> entry[0] === key)
-    if (foundIndex === -1) return false
-    bucket.splice(foundIndex,1)
-    return true
+    const bucket = this.buckets[index];
+    const foundIndex = bucket.findIndex((entry) => entry[0] === key);
+    if (foundIndex === -1) return false;
+    bucket.splice(foundIndex, 1);
+    return true;
   }
 
   length() {
-
-    return this.buckets.reduce((count, bucket)=> {
-        return count + bucket.length
-    },0)
+    return this.buckets.reduce((count, bucket) => {
+      return count + bucket.length;
+    }, 0);
   }
- 
+
+  entries() {
+    return this._getAllEntries();
+  }
+
+  keys() {
+    return this._getAllEntries().map((entry) => entry[0]);
+  }
+
+  values() {
+    return this._getAllEntries().map((entry) => entry[1]);
+  }
+
   _hash(key) {
     let hashCode = 0;
     const primeNumber = 31;
@@ -78,5 +88,9 @@ class HashMap {
       hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
     return hashCode;
+  }
+
+  _getAllEntries() {
+    return this.buckets.flatMap((bucket) => bucket);
   }
 }
